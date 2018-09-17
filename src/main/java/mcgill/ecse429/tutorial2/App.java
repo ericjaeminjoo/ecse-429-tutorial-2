34b929f98ad8efa7da928917a79d2870d4691e1b
Eric Joo <ericjaeminjoo@gmail.com>
2018-09-17 15:50:15 -0400

    Initial commit

---
+++ src/main/java/mcgill/ecse429/tutorial2/App.java
@@ -0,0 +1,13 @@
+package mcgill.ecse429.tutorial2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
